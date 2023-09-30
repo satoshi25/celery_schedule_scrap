c51148077d8277f7771b76121c3a0296db0b7a05
satoshi25 <81741466+satoshi25@users.noreply.github.com>
2023-09-30 16:18:30 +0900

    feat: Add handle_disconnect function - DB와 연결되지 않았을때 재연결을 시도하도록 handle_disconnect 추가

--- src/database/connection.py
+++ src/database/connection.py
@@ -1,12 +1,26 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import DisconnectionError
 from dotenv import load_dotenv
 
 import os
+import logging
 
 load_dotenv()
 
+
+def handle_disconnect(dbapi_connection, connection_record):
+    if dbapi_connection is not None:
+        try:
+            dbapi_connection.ping(reconnect=True)
+        except Exception as e:
+            logging.error(f"Database disconnection error: {e}, Connection record: {connection_record}")
+            raise DisconnectionError("Could not reconnect: %s" % e)
+
+
 DB_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DB_URL, echo=True)
+
+event.listen(engine, 'handle_error', handle_disconnect)
 
 SessionFactory = sessionmaker(autoflush=False, autocommit=False, bind=engine)
